a2ff16719dcdfa926babfb5144f8f3699b01d278
Max <maxageev5@gmail.com>
2022-11-12 19:50:44 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,22 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public void getDataFromFile(File file) {
+        try(FileInputStream input = new FileInputStream(file)){
+
+            int c;
+            while ((c=input.read()) != 1){
+                System.out.println((char)c);
+            }
+
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
