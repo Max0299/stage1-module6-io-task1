a4dd6478d39d6b49b0b90a603290a8b2927785bc
Max <maxageev5@gmail.com>
2022-11-13 12:37:14 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -7,16 +7,33 @@ import java.io.IOException;
 
 public class FileReader {
 
-    public void getDataFromFile(File file) {
+    public Profile getDataFromFile(File file) {
+        StringBuilder str = new StringBuilder();
         try(FileInputStream input = new FileInputStream(file)){
 
             int c;
-            while ((c=input.read()) != 1){
-                System.out.println((char)c);
+            while ((c = input.read()) != -1){
+                str.append((char) c);
             }
-
-        }catch (IOException ex){
-            System.out.println(ex.getMessage());
         }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+
+        String[] array = str.toString().split("\n");
+
+        String name = array[0];
+        String age = array[1];
+        String email = array[2];
+        String phone = array[3];
+
+        String[] name1 = email.split(": ");
+        String[] age1 = email.split(": ");
+        String[] email1 = email.split(": ");
+        String[] phone1 = email.split(": ");
+
+        Profile profile = new Profile(name1[1],Integer.parseInt(age1[1]),email1[1],Long.parseLong(phone1[1]));
+
+        return profile;
     }
 }
